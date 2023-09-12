1e53fb01e0170b24dd18d4a6c6dca60b1731cc71
Balazs Bereczky <balazs.bereczky@wecanconsulting.eu>
2023-09-12 10:33:16 +0200

    Initial Commit

---
+++ news/newsApp.swift
@@ -0,0 +1,17 @@
+//
+//  newsApp.swift
+//  news
+//
+//  Created by Balazs Bereczky on 2023. 09. 12..
+//
+
+import SwiftUI
+
+@main
+struct newsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
